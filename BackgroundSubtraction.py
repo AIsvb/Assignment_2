03e8257efb29d893c58bcb6da820ca058465f13a
Gino Kuiper <gino.kuiper@gmail.com>
2023-02-27 21:39:52 +0100

    Finished background subtractor

--- BackgroundSubtraction.py
+++ BackgroundSubtraction.py
@@ -21,24 +21,23 @@ class BackgroundSubtractor:
 
             # Read video, subtract background without training the background model
             ret, frame = video.read()
-            # frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
             foreground = self.fgbg.apply(frame, None, 0)
             if frame is None:
                 break
 
             # Postprocessing
-            # fgmask = cv2.erode(foreground, self.kernel, iterations=2)
-            # fgmask[np.abs(foreground) < 254] = 0
-            # fgmask = cv2.dilate(fgmask, self.kernel, iterations=1)
-            fgmask = self.find_contours(foreground)
+            foreground[np.abs(foreground) < 254] = 0
+            # foreground = cv2.erode(foreground, self.kernel, iterations=1)
+            # foreground = cv2.dilate(foreground, self.kernel, iterations=1)
+            fgmask = self.draw_contours(foreground)
 
             # Show video
-            # cv2.imshow('Frame', frame)
-            cv2.imshow('Unprocessed', foreground)
+            cv2.imshow('Frame', frame)
+            # cv2.imshow('Unprocessed', foreground)
             cv2.imshow('Processed', fgmask)
 
             keyboard = cv2.waitKey(1)
-            if keyboard == 'q' or keyboard == 27:
+            if keyboard == 'q':
                 break
 
         video.release()
@@ -46,35 +45,39 @@ class BackgroundSubtractor:
         cv2.destroyAllWindows()
         cv2.waitKey(0)
 
-    # Functionality for post-processing: now a duplicate of what already is in subtract_background
-    def post_process(self, img):
-        # e_shape = cv2.MORPH_RECT
-        e_shape = cv2.MORPH_CROSS
-        # e_shape = cv2.MORPH_ELLIPSE
-        e_size = 1
-        e_element = cv2.getStructuringElement(e_shape, (2 * e_size + 1, 2 * e_size + 1),
-                                           (e_size, e_size))
-
-        # d_shape = cv2.MORPH_RECT
-        # d_shape = cv2.MORPH_CROSS
-        d_shape = cv2.MORPH_ELLIPSE
-        d_size = 1
-        d_element = cv2.getStructuringElement(d_shape, (2 * d_size + 1, 2 * d_size + 1),
-                                           (d_size, d_size))
-
-        img[np.abs(img) < 250] = 0
-        img = cv2.erode(img, e_element, iterations=1)
-        # img = cv2.dilate(img, d_element, iterations=1)
-
-        return img
-
     # Functionality to find and draw contours on a frame
-    def find_contours(self, image):
+    def draw_contours(self, image):
         ret, thresh = cv2.threshold(image, 127, 255, 0)
         contours, hierarchy = cv2.findContours(thresh, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
-        contours = sorted(contours, key=cv2.contourArea)
+        
+        # Create array containing the index, area and parent of every contour and sort it in a large-small order
+        contours2 = np.zeros((len(contours), 3))
+        for i in range(len(contours)):
+            contours2[i][0] = i
+            contours2[i][1] = float(cv2.contourArea(contours[i]))
+            contours2[i][2] = hierarchy[0][i][3]
+        contours2 = contours2[contours2[:, 1].argsort()][::-1]
 
-        fgmask = np.zeros_like(image)
-        cv2.drawContours(fgmask, [contours[-1]], -1, (255, 255, 255), 1)
+        # Sort contours in a large-small order
+        contours = sorted(contours, key=cv2.contourArea, reverse=True)
 
-        return fgmask
+        # Make copy of the input image to draw the contours on
+        image_copy = np.zeros_like(image)
+
+        # Based on camera, set threshold for drawn contour sizes
+        if self.video == 'data/cam1/video.avi':
+            area = 55
+        elif self.video == 'data/cam2/video.avi':
+            area = 26
+        elif self.video == 'data/cam3/video.avi':
+            area = 137
+        else:
+            area = 25
+
+        # Draw contours of the subject on the copy image
+        cv2.drawContours(image_copy, [contours[0]], -1, (255, 255, 255), thickness=cv2.FILLED)
+        for i in range(1, len(contours)):
+            if contours2[i][1] > area:
+                cv2.drawContours(image_copy, [contours[i]], -1, (0, 0, 0), thickness=cv2.FILLED)
+
+        return image_copy
--- Test_Gino.py
+++ Test_Gino.py
@@ -1,6 +1,13 @@
 import cv2
 from BackgroundSubtraction import BackgroundSubtractor as BS
 
-bs = BS('data/cam3/background.avi', 'data/cam3/video.avi')
-bs.subtract_background()
+bs1 = BS('data/cam1/background.avi', 'data/cam1/video.avi')
+bs2 = BS('data/cam2/background.avi', 'data/cam2/video.avi')
+bs3 = BS('data/cam3/background.avi', 'data/cam3/video.avi')
+bs4 = BS('data/cam4/background.avi', 'data/cam4/video.avi')
+
+bs1.subtract_background()
+# bs2.subtract_background()
+# bs3.subtract_background()
+# bs4.subtract_background()
 
